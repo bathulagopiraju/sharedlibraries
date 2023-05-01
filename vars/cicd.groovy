2df62f85aa96c252e3e956f38d67fdbafaf26019
bathulagopiraju <130970876+bathulagopiraju@users.noreply.github.com>
2023-05-01 22:52:51 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,12 +6,12 @@ def newGit(repo)
 
 def newMaven()
 {
-   sh 'mvn package'
+   sh'mvn package'
 }
 
 def new Deploy(jobname,ipaddress,context)
 {
-   sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war'
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
 def executedSelenium(jobname)
